862229cef1d2f093181301be4ab4f09d16d2c99a
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-04-03 19:52:07 -0300

    feat: api login confirmation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,6 @@
-import { DeepMap, FieldError, FieldValues, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-
 import { lineOfDogs, logoHorizontal, passwordEye } from '@/assets/icons'
 
 import {
@@ -17,36 +16,67 @@ import {
 } from './styles'
 import { API_BASE_URL } from '@/config'
 
-export type FieldErrors<TFieldValues extends FieldValues = FieldValues> =
-  DeepMap<TFieldValues, FieldError>
-
 export function Login() {
-  const schema = z.object({
+  const schemaLogin = z.object({
     email: z.string().email({ message: 'E-mail incorreto' }),
     password: z.string().min(5, { message: 'Minimo de 5 digitos para senha' }),
   })
 
+  type LoginForm = z.infer<typeof schemaLogin>
+
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
-  }: FieldErrors = useForm({
-    resolver: zodResolver(schema),
+  } = useForm<LoginForm>({
+    resolver: zodResolver(schemaLogin),
   })
 
-  // const onSubmit = (data) => console.log(data.email)
-  async function handleLogin(data) {
+  async function sendLoginRequest(data: LoginForm) {
+    const apiUrl = `${API_BASE_URL}/auth/sessions`
+
     const requestBody = {
       email: data.email,
       password: data.password,
     }
-    await fetch(`${API_BASE_URL}/auth/sessions`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestBody),
-    })
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      })
+
+      if (response.status === 401) {
+        setError('root.serverError', {
+          type: '401',
+          message: 'Credenciais inválidas',
+        })
+      }
+
+      if (!response.ok) {
+        throw new Error(
+          `Error making request to ${apiUrl}. Method: POST. Status code: ${response.status}.`,
+        )
+      } else {
+        const json = await response.json()
+        return json
+      }
+    } catch (error) {
+      if (error instanceof Error)
+        console.error(
+          `An error occurred while making the request to ${apiUrl}. Error message:  ${error.message}`,
+        )
+      return null
+    }
+  }
+
+  async function handleLogin(data: LoginForm) {
+    const response = await sendLoginRequest(data)
+    console.log(response)
   }
 
   function handleRegisterOrganization() {
@@ -93,9 +123,10 @@ export function Login() {
             </div>
 
             <Buttons>
-              <Button type="submit" onClick={() => {}} className="primary">
+              <Button type="submit" className="primary">
                 Login
               </Button>
+              <ErrorMessage>{errors.root?.serverError.message}</ErrorMessage>
               <Button type="button" onClick={() => {}} className="secondary">
                 Cadastrar minha organização
               </Button>
